5b48d444db607ba59da9993e608203b80ae8534f
Felix Lepistö <felix.lepisto@lbs.se>
2018-11-29 16:12:16 +0100

    Init

---
+++ Assets/ChaserCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserCamera : MonoBehaviour
+{
+    public Transform target;
+
+    public enum ChaseMode { Linear, Square, Cubic, Exponential }
+    public ChaseMode chase;
+    public float speed = 1f;
+
+    private new Camera camera;
+    private Vector3 offset;
+
+    public Vector3 CameraPosition
+    {
+        get { return transform.position + offset; }
+        set { transform.position = value + offset; }
+    }
+
+    private void Start()
+    {
+        offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            float dist = Vector3.Distance(CameraPosition, target.position);
+            CameraPosition = Vector3.MoveTowards(CameraPosition, target.position, DistanceValue(dist, chase));
+        }
+    }
+
+    private float DistanceValue(float distance, ChaseMode mode)
+    {
+        switch (mode)
+        {
+            case ChaseMode.Linear:
+                return speed * Time.deltaTime;
+            case ChaseMode.Square:
+                return Mathf.Pow(distance, 2) * Time.deltaTime;
+            case ChaseMode.Cubic:
+                return Mathf.Pow(distance, 3) * Time.deltaTime;
+            case ChaseMode.Exponential:
+                return Mathf.Pow(2, distance) * Time.deltaTime;
+            default:
+                return 0;
+                break;
+        }
+    }
+}
---
+++ Assets/FishMovement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class FishMovement : MonoBehaviour
+{
+    protected Vector2 targetDirection;
+    public float acceleration = 24f;
+    public float deceleration = 64f;
+    public float dashSpeed = 64f;
+    public float maxSpeed = 8f;
+
+    public float rotationSpeed = 720f;
+
+    protected SpriteRenderer rend;
+    protected Collider2D coll;
+    protected Rigidbody2D rbody;
+
+
+    protected Vector2 lastVelocityDirection;
+
+    protected Color tint;
+
+    protected virtual void Awake()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        coll = GetComponent<Collider2D>();
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        ApplyTint();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        targetDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Move();
+        RotateSprite();
+    }
+
+    protected virtual Vector2 GetWantedVector()
+    {
+        return targetDirection.normalized * maxSpeed;
+    }
+
+    protected virtual void RotateSprite()
+    {
+        Vector2 vel = rbody.velocity;
+
+        if (vel.magnitude > 0.1f)
+        {
+            lastVelocityDirection = rbody.velocity.normalized;
+            rend.flipY = lastVelocityDirection.x < 0 ? true : false;
+
+            float ang = Mathf.Atan2(lastVelocityDirection.y, lastVelocityDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = SmoothRotateTo(Quaternion.Euler(0, 0, ang));
+        }
+    }
+
+    private Quaternion SmoothRotateTo(Quaternion target)
+    {
+
+        Quaternion current = transform.rotation;
+
+        return Quaternion.RotateTowards(current, target, rotationSpeed * Time.deltaTime);
+
+    }
+
+    protected virtual void Move()
+    {
+        if (rbody.velocity.magnitude > maxSpeed)
+        {
+            rbody.velocity = Vector2.MoveTowards(rbody.velocity, GetWantedVector(), deceleration * Time.deltaTime);
+        }
+        else
+        {
+            rbody.velocity = Vector2.MoveTowards(rbody.velocity, GetWantedVector(), acceleration * Time.deltaTime);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rbody.velocity = lastVelocityDirection * dashSpeed;
+        }
+    }
+
+    protected virtual void ApplyTint()
+    {
+        rend.color = GetTint();
+    }
+
+    protected virtual Color GetTint()
+    {
+        return Random.ColorHSV(0, 1, 0, 0.2f, 0.8f, 1f, 1f, 1f);
+    }
+}
